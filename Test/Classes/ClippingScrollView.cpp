c0913c2ce8663d4adcb00e72b7f889cfd7631511
이한밝 <leehb@aceproject.co.kr>
2020-08-21 17:05:12 +0900

    dd

---
+++ Test/Classes/ClippingScrollView.cpp
@@ -0,0 +1,8 @@
+//
+//  ClippingScrollView.cpp
+//  Test
+//
+//  Created by ace-22 on 2020/08/21.
+//
+
+#include "ClippingScrollView.hpp"
---
+++ Test/Classes/ClippingScrollView.h
@@ -0,0 +1,12 @@
+//
+//  ClippingScrollView.h
+//  Test
+//
+//  Created by ace-22 on 2020/08/21.
+//
+
+#ifndef ClippingScrollView_h
+#define ClippingScrollView_h
+
+
+#endif /* ClippingScrollView_h */
